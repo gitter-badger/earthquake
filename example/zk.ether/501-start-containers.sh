e786b777c17402dee4ab9a57820d2e0d8cfc0d5b
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2015-06-03 15:00:52 +0900

    fix typos

--- example/zk.ether/501-start-containers.sh
+++ example/zk.ether/501-start-containers.sh
@@ -3,5 +3,5 @@
 set -x
 
 for f in $(seq 1 3); do \
-        docker run -i -t -d -e ZKID=${f} -h zk${f} --name zk${f} zk_testbed /bin/bash; \
+        docker run -i -t -d -e ZKID=${f} -e ZKENSEMBLE=1 -h zk${f} --name zk${f} zk_testbed /bin/bash; \
       done
