d834a29930edaf16d1b5cc8e7e15cab4f37bcb5f
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2015-06-05 14:55:28 +0900

    Add log of found-zk-weird-boot-20150605, although this is not directly related to ZOOKEEPER-2172.

--- example/zk.ether/601-reconfig.sh
+++ example/zk.ether/601-reconfig.sh
@@ -5,11 +5,14 @@ set -x
 ZK=../zk/zookeeper
 
 echo '=== CREATE HOGE ==='
-$ZK/bin/zkCli.sh -server 192.168.42.1 create /hoge fubar
+$ZK/bin/zkCli.sh -server 192.168.42.3 create /hoge fubar
 
-echo '=== RECONFIG ==='
-$ZK/bin/zkCli.sh -server 192.168.42.1 reconfig -add server.2=192.168.42.2:2888:3888\;2181 &
-$ZK/bin/zkCli.sh -server 192.168.42.1 reconfig -add server.3=192.168.42.3:2888:3888\;2181
+echo '=== SYNC & RECONFIG ==='
+$ZK/bin/zkCli.sh -server 192.168.42.1 reconfig -add server.2=192.168.42.2:2888:3888\;2181
+$ZK/bin/zkCli.sh -server 192.168.42.3 create /fuga barfu
+$ZK/bin/zkCli.sh -server 192.168.42.3 sync / &
+$ZK/bin/zkCli.sh -server 192.168.42.1 reconfig -add server.3=192.168.42.3:2888:3888\;2181 &
+
 
 echo '=== WAIT ==='
 sleep 10
@@ -19,3 +22,4 @@ $ZK/bin/zkCli.sh -server 192.168.42.1 config
 
 echo '=== GET HOGE ==='
 $ZK/bin/zkCli.sh -server 192.168.42.1 get /hoge
+$ZK/bin/zkCli.sh -server 192.168.42.1 get /fuga
